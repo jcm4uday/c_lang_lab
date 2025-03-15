fbaedba8b30721d2bb87332b3aa1dd2797ce67ee
jcm4uday <jcm4uday@gmail.com>
2025-03-15 23:35:40 +0000

    cha2 ex_5 working

--- Chap_2/ex_5.c
+++ Chap_2/ex_5.c
@@ -2,3 +2,33 @@
 where any character from the string s2 occurs, or -1 if s1 contains no characters from s2. 
 (The standard library function strpbrk does the same job but returns a pointer to the 
 location.)  */
+
+#include <stdio.h>
+
+int any(char s1[], char s2[]);
+
+void main(){
+
+    char st1[] = "king";
+    char st2[] = "bea";
+
+    int val = any(st1, st2);
+    printf("string location of s1: %d\n", val);
+
+
+}
+
+int any(char s1[], char s2[]){
+
+    int i;
+    int j;
+    for(i =0; s1[i] !='\0'; i++){
+        for(j=0; s2[j] !='\0'; j++){
+            if(s1[i] == s2[j]){
+                printf("first hit and the index of s1 was: %d\n", i);
+                return i;
+            }
+        }
+    }
+    return -1;
+}
